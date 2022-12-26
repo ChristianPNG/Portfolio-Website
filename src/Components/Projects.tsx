e04b6726d0cf96b1c9bef5c959ada09835ceb5ce
Christian Rullan <laptopchris01@gmail.com>
2022-12-26 10:40:03 -0500

    shine hover effect on boxes

--- src/Components/Projects.tsx
+++ src/Components/Projects.tsx
@@ -8,12 +8,30 @@ export function Project(): JSX.Element {
 			className="projects-container"
 		>
 			<div className="grid">
-				<div className="grid-item">Item 1</div>
-				<div className="grid-item">Item 2</div>
-				<div className="grid-item">Item 3</div>
-				<div className="grid-item">Item 4</div>
-				<div className="grid-item">Item 5</div>
-				<div className="grid-item">Item 6</div>
+				<div>
+					<div></div>
+					<div></div>
+				</div>
+				<div>
+					<div></div>
+					<div></div>
+				</div>
+				<div>
+					<div></div>
+					<div></div>
+				</div>
+				<div>
+					<div></div>
+					<div></div>
+				</div>
+				<div>
+					<div></div>
+					<div></div>
+				</div>
+				<div>
+					<div></div>
+					<div></div>
+				</div>
 			</div>
 		</div>
 	);
